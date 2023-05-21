0fbe3903ba505935ff9a10b0c2a0c0ad73aed6de
김민수 MINSOO KIM <alstn113@gmail.com>
2023-05-21 14:29:43 +0900

    progress bar

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+import ProgressBar from "../components/common/ProgressBar/ProgressBar";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <div>
+      <div>
+        <ProgressBar duration="10s" animation={toggle ? "play" : "done"} />
+      </div>
+      <div>
+        <button onClick={() => setToggle(!toggle)}>Toggle</button>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
